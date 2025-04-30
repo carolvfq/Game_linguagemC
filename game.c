4465c8947ed0db9514ab1b85fb71c69d3f8eba0d
carolvfq <carolvfdq@gmail.com>
2025-04-30 13:06:34 -0300

    estruturando o projeto

---
+++ game.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+typedef struct{
+    int valo1;
+    int valor2;
+    int resultado;
+    int operacao;
+}calcular;
+
+void jogar();
+void mandar_info(calcular calc);
+int pontos=0;
+
+int main(){
+srand(time(NULL));
+jogar();
+
+    return 0;
+}
+
+void jogar(){
+
+}
+
+void mandar_info(calcular calc){
+
+}
+
+soma(int respostas,calcular calc){
+
+}
+
+subtracao(int respostas,calcular calc){
+
+}
+
+multiplicacao(int respostas,calcular calc){
+
+}
