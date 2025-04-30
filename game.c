5791a97b4fb7a820500e3eee1a63eacd9928ed8d
carolvfq <carolvfdq@gmail.com>
2025-04-30 15:07:57 -0300

    implementando a funcao mostrar_info e funcao somar

--- game.c
+++ game.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 typedef struct{
-    int valo1;
+    int valor1;
     int valor2;
     int resultado;
     int operacao;
+    int dificuldade;
 }calcular;
 
 void jogar();
-void mandar_info(calcular calc);
+void mostrar_info(calcular calc);
 int pontos=0;
 
 int main(){
@@ -21,11 +22,37 @@ void jogar(){
 
 }
 
-void mandar_info(calcular calc){
+void mostrar_info(calcular calc){
+   char op[25];
+   if (calc.operacao == 0){
+     sprintf(op, "somar");
+   }
+   else if (calc.operacao == 1){
+    sprintf(op, "subtracao");
+  }
+   else if (calc.operacao == 2){
+    sprintf(op, "multiplicacao");
+  }
+  else {
+    sprintf(op, "operacao invalida");
+  }
 
+  printf("valor1: %d\n , valor2: %d\n, dificuldade: %d\n , operacao: %s\n", calc.valor1, calc.valor2, calc.dificuldade,op);
 }
 
 soma(int respostas,calcular calc){
+  int resultado = calc.valor1 + calc.valor2;
+  calc.resultado= resultado;
+  int certo = 0; 
+  if(resultado == calc.resultado){
+    printf("A resposta esta correta!\n");
+    certo= 1;
+  }
+  else{
+   printf("A resposta esta errada!\n");
+  }
+printf("%d + %d = %d", calc.valor1, calc.valor2, calc.resultado);
+return certo;
 
 }
 
