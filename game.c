e2be16730c61fa5a8db3c147661219c5c0e64450
carolvfq <carolvfdq@gmail.com>
2025-05-01 13:58:37 -0300

    implementando as funçoes subtracao , multiplicacao e jogar

--- game.c
+++ game.c
@@ -19,6 +19,61 @@ jogar();
 }
 
 void jogar(){
+  calcular calc;
+  int dificuldade;
+
+  printf("informe o valor da dificuldade da operacao matematica [1, 2, 3 ou 4]:\n");
+  scanf("%d",&dificuldade);
+  calc.dificuldade= dificuldade;
+
+// gera um numero rand0mico ente 0 e 2
+  calc.operacao= rand () % 3;
+    
+  if( calc.dificuldade==1){
+    calc.valor1= rand () % 11;
+    calc.valor2= rand () % 11;
+    
+  }
+  else if( calc.dificuldade==2){
+    calc.valor1= rand () % 101;
+    calc.valor2= rand () % 101;
+    
+  }
+  else if( calc.dificuldade==3){
+    calc.valor1= rand () % 1001;
+    calc.valor2= rand () % 1001;
+    
+  }
+  else if( calc.dificuldade==4){
+    calc.valor1= rand () % 10001;
+    calc.valor2= rand () % 10001;
+    
+  }
+  else {
+    calc.valor1= rand () % 100001;
+    calc.valor2= rand () % 100001;
+    
+  }
+  printf("digite o resultado para a seguinte operacao:\n");
+  int resposta;
+  if (calc.operacao == 0){
+   printf("qual o valor de %d + %d:");
+   scanf("%d", &resposta);
+    if(somar(resposta,calc)){
+      pontos= pontos + 1;
+      printf("voce tem %d pontos\n",pontos);
+    }
+  }
+  else if (calc.operacao == 1){
+   
+ }
+  else if (calc.operacao == 2){
+   
+ }
+ else {
+  
+ }
+
 
 }
 
@@ -57,9 +112,31 @@ return certo;
 }
 
 subtracao(int respostas,calcular calc){
-
+  int resultado = calc.valor1 - calc.valor2;
+  calc.resultado= resultado;
+  int certo = 0; 
+  if(resultado == calc.resultado){
+    printf("A resposta esta correta!\n");
+    certo= 1;
+  }
+  else{
+   printf("A resposta esta errada!\n");
+  }
+printf("%d - %d = %d", calc.valor1, calc.valor2, calc.resultado);
+return certo;
 }
 
 multiplicacao(int respostas,calcular calc){
-
+  int resultado = calc.valor1 * calc.valor2;
+  calc.resultado= resultado;
+  int certo = 0; 
+  if(resultado == calc.resultado){
+    printf("A resposta esta correta!\n");
+    certo= 1;
+  }
+  else{
+   printf("A resposta esta errada!\n");
+  }
+printf("%d + %d = %d", calc.valor1, calc.valor2, calc.resultado);
+return certo;
 }
